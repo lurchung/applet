dec80d421739e18b74ef2260c0ea4cc1a1953825
道阻且长 <luchang@stu.ouc.edu.cn>
2024-03-19 23:52:40 +0800

    update

---
+++ pages/Home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="Home">
+		<navigator url="/pages/person/person">个人信息</navigator>
+		<navigator url="/pages/register/register">登录</navigator>
+		<navigator url="/pages/schedule/schedule">日程</navigator>
+		<navigator url="/pages/timetable/timetable">课表页</navigator>
+		<navigator url="/pages/new-schedule/new-schedule">新建日程</navigator>
+		<navigator url="/pages/Home/Home">主页</navigator>
+
+	</view>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style lang="less">
+
+</style>
